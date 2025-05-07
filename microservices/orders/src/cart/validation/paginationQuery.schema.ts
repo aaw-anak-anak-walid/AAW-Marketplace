1bfc33dd043a3af8295d3d6c2deef655867007d2
reyhanwiyasa <reyhanwiyasa@gmail.com>
2025-05-07 10:36:31 +0700

    feat: implement cart pagination

---
+++ microservices/orders/src/cart/validation/paginationQuery.schema.ts
@@ -0,0 +1,9 @@
+// src/routes/product.validation.ts
+import { z } from "zod";
+
+export const paginationQuerySchema = z.object({
+  query: z.object({
+    page: z.coerce.number().int().min(1).default(1),
+    limit: z.coerce.number().int().min(1).max(100).default(10),
+  }),
+});
