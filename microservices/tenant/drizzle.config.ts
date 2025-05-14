2adbe771ae5be7d992671d692461c2796a5a4600
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-05-14 15:45:39 +0700

    feat: integrate db and cache to aws

--- microservices/tenant/drizzle.config.ts
+++ microservices/tenant/drizzle.config.ts
@@ -11,5 +11,6 @@ export default {
     user: process.env.DB_USER ?? "postgres",
     password: process.env.DB_PASSWORD ?? "postgres",
     database: process.env.DB_NAME ?? "postgres",
+    ssl: process.env.DB_SSL === 'true' ? true : false,
   },
 } satisfies Config;
